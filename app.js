7b36bf0e730a7e1a2529eecd5d0aafb1a077ea1a
Pamela <PamelaYuhas@gmail.com>
2018-04-16 14:09:46 -0600

    Galv-Investigators-3

---
+++ app.js
@@ -0,0 +1,39 @@
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart (ctx, {
+type: 'line',
+
+data: {
+  labels: ["2014", "2015", "2016"],
+  datasets: [{
+      label: '# of Votes',
+      data: [73, 199, 395],
+      backgroundColor: [
+          'rgba(54, 162, 235, .2)'
+      ],
+      borderColor: [
+          'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1,
+  }]
+},
+options: {
+        scales: {
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Year'
+                }
+            }],
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Students'
+                },
+            ticks: {
+            beginAtZero: true
+            }
+            }]
+        },
+      }
+})
